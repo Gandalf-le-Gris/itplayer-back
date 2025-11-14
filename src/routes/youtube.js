c9003dd3233b5c1433af220952fab2ca500c8cb0
Antonin Bouhier <abouhier@oxyl.fr>
2025-11-14 16:58:19 +0100

    Test API audioStreams

--- src/routes/youtube.js
+++ src/routes/youtube.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const youtubeService = require("../services/youtube.ts").default;
+const { execa } = require("execa");
 
 router.get("/search", async (req, res) => {
   try {
     res.json(await youtubeService.search(req.query.query));
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({});
+  }
+});
+
+router.get("/play", async (req, res) => {
+  try {
+    const { audioFormat, stream } = await youtubeService.getAudioStream(req.query.videoId);
+    res.setHeader('Content-Type', audioFormat.mime_type);
+    res.setHeader('Content-Disposition', `inline; filename="audio.${audioFormat.mime_type.split('/')[1]}"`);
+    res.pipe(stream);
   } catch (err) {
     console.error(err);
     res.status(500).json({});
--- src/services/youtube.ts
+++ src/services/youtube.ts
@@ -1,17 +1,49 @@
-import { Innertube } from 'youtubei.js';
+import { VM } from 'vm2';
+import { Innertube, UniversalCache } from 'youtubei.js';
 
 class YoutubeService {
   private yt: Innertube | undefined;
 
   public async init() {
-    this.yt = await Innertube.create();
+    const vm: VM = new VM({
+      timeout: 1000,
+      sandbox: {},
+    });
+    const cache: UniversalCache = new UniversalCache(false);
+    this.yt = await Innertube.create({
+      cache,
+      retrieve_player: true,
+      // @ts-ignore
+      execute: vm.run.bind(vm),
+    });
   }
 
   public async search(query: string) {
-      return await this.yt?.search(query, {
-        type: 'video',
-        sort_by: 'view_count'
-      }) ?? [];
+    return await this.yt?.search(query, {
+      type: 'video',
+      sort_by: 'relevance',
+    }) ?? [];
+  }
+
+  public async getAudioStream(videoId: string) {
+    const videoInfo = await this.yt?.getInfo(videoId);
+    const audioFormat = videoInfo?.chooseFormat({
+      type: 'audio',
+      quality: 'best',
+      format: 'audio',
+      client: 'ANDROID',
+    });
+    if (!audioFormat) {
+      throw new Error('error.audio.not-found');
+    }
+    console.log(audioFormat)
+    const stream = this.yt?.download(videoId, {
+      type: 'audio',
+      quality: 'best',
+      format: 'audio',
+      client: 'ANDROID',
+    });
+    return { audioFormat, stream };
   }
 }
 
